3600efe7346855f59cd48736890d0d48b2523d0a
kadithpaternina <kadithpaternina@gmail.com>
2025-10-16 19:11:06 -0500

    Cambio

--- css/operacion.js
+++ css/operacion.js
@@ -7,26 +7,35 @@ function obtenerNumeros() {
 // Funciones matemáticas
 function suma() {
   const [n1, n2] = obtenerNumeros();
-  document.getElementById("resultado").textContent = n1 + n2;
+  if (n1 === " " ){
+        document.getElementById("resultado").innerText = "Falta ingresar un número.";
+  } 
+  else {
+  document.getElementById("resultado").value = n1 + n2;
+}
 }
 
 function resta() {
   const [n1, n2] = obtenerNumeros();
-  document.getElementById("resultado").textContent = n1 - n2;
+  if(n1>n2){
+  document.getElementById("resultado").value = n1 - n2;
 }
-
+else{
+ alert("No se puede Restar");
+}
+}
 function division() {
   const [n1, n2] = obtenerNumeros();
   if (n2 === 0) {
     alert("No se puede dividir entre 0");
     return;
   }
-  document.getElementById("resultado").textContent = n1 / n2;
+  document.getElementById("resultado").value = n1 / n2;
 }
 
 function multiplicacion() {
   const [n1, n2] = obtenerNumeros();
-  document.getElementById("resultado").textContent = n1 * n2;
+  document.getElementById("resultado").value = n1 * n2;
 }
 
 // Asignar funciones a los botones
